606fbbb2912f85b9ad5d13b6558b8e202123167c
Gabriel <gaby_lamuerte@hotmail.com>
2023-09-18 18:45:00 -0300

    first commit

---
+++ assets/JS/color.bundle.js
@@ -0,0 +1,8 @@
+var color = document.querySelector('#color');
+var salida = document.querySelector('#salida');
+color.addEventListener("input", function () {
+  var value = color.value;
+  salida.innerHTML = value;
+  salida.style.background = value;
+  salida.style.color = "#fff";
+});
